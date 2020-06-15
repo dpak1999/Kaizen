6275511742da05c1563b90b749180a748f23ec94
dpak1999 <deepakkumardash313@gmail.com>
2020-06-15 17:11:57 +0530

    creating dashboard notification, project list comp

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class ProjectList extends Component {
+  render() {
+    return (
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by deepak</p>
+            <p className="grey-text">3rd September, 5Am</p>
+          </div>
+        </div>
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by deepak</p>
+            <p className="grey-text">3rd September, 5Am</p>
+          </div>
+        </div>
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by deepak</p>
+            <p className="grey-text">3rd September, 5Am</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectList;
