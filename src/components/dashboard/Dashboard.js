5aaf9ba96c6222c5f1b592374d594d2d783aa62b
dpak1999 <deepakkumardash313@gmail.com>
2020-06-16 12:02:28 +0530

    connecting project componet to redux store

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import ProjectList from "../projects/ProjectList";
 import Notifications from "./Notifications";
 class Dashboard extends Component {
   render() {
+    // console.log(this.props);
+    const { projects } = this.props;
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -18,5 +21,8 @@ class Dashboard extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return { projects: state.project.projects };
+};
 
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
--- src/store/reducer/projectReducer.js
+++ src/store/reducer/projectReducer.js
@@ -1,4 +1,24 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "dummy text 1",
+      content: "Occaecat incididunt dolor ad nostrud aute.",
+    },
+    {
+      id: "2",
+      title: "dummy text 3",
+      content:
+        "Duis minim non proident laborum commodo consequat consectetur esse laboris nisi.",
+    },
+    {
+      id: "3",
+      title: "dummy text 3",
+      content:
+        "Incididunt culpa dolor nostrud fugiat nisi elit enim dolore veniam Lorem ipsum exercitation consectetur.",
+    },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
