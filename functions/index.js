d71ad570e054f78f066816c6a0fd282439e9a34e
dpak1999 <deepakkumardash313@gmail.com>
2020-06-17 20:31:32 +0530

    creating user function

--- functions/index.js
+++ functions/index.js
@@ -26,3 +26,15 @@ exports.projectCreated = functions.firestore
     };
     return createNotification(notification);
   });
+
+exports.userJoined = functions.firestore
+  .document("users/{uid}")
+  .onCreate((doc) => {
+    const user = doc.data();
+    const notification = {
+      content: "Joined the party",
+      user: `${user.firstName} ${user.lastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    };
+    return createNotification(notification);
+  });
